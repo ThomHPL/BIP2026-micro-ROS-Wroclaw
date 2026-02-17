4f67a747e48a854017a9760e0421284bf06d5439
thomas.herpoel@gmail.com <thomas.herpoel@gmail.com>
2026-02-17 14:07:01 +0100

    adding sources

---
+++ microros_ws/build/micro_ros_agent/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/antunes2002/microros_ws/src/micro-ros-agent/micro_ros_agent
+# Build directory: /home/antunes2002/microros_ws/build/micro_ros_agent
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
